c35723cb06429faf2942cab1257486a755235f88
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-04-06 16:48:02 +1000

    JS - Current Weather done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // List of all Variables
-var apiKey = "34294986534f43665a27b0eb1f9cf2fd"
-var currentDay = moment().format("dddd, MMMM Do YYYY");
+var apiKey = "34294986534f43665a27b0eb1f9cf2fd";
+var currentDay = moment().format("MMM Do YY");
 var searchButton = $("#searchBtn");
 var searchHistoryList =[];
 
 
 // SEARCH SECTION
- 
+
  // Search button event listener
  searchButton.on("click", function(event) {
      event.preventDefault();
@@ -16,13 +16,14 @@ var searchHistoryList =[];
 
      if (!searchHistoryList.includes(city)){
          searchHistoryList.push(city);
-         var searchHistory = $(`<li>${city}</li>`).addClass("list-group-item list-group-item-action");
+         var searchHistory = $(`
+         <li>${city}</li>
+         `).addClass("list-group-item list-group-item-action");
          $("#searchedCities").append(searchHistory);
      };
 
      localStorage.setItem("city", JSON.stringify(searchHistoryList));
-     console.log(searchHistoryList);
- })
+ });
 
     
 
@@ -30,7 +31,26 @@ var searchHistoryList =[];
 
  // Current Weather
  function currentWeather(city) {
-     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}";
+    var currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    $.ajax({
+        url: currentWeatherURL,
+        method: "GET",
+    }).then (function(weatherResponse) {
+        //console.log(weatherResponse);
 
- }
+        $("#resultSection").css("display", "block");
+        $("#currentWeather").empty();
 
+        var iconID = weatherResponse.weather[0].icon;
+        var iconURL = `http://openweathermap.org/img/wn/${iconID}.png`;
+        var currentCity = $(`
+        <h2 id="currentCity">${weatherResponse.name} <img src="${iconURL}" alt="${weatherResponse.weather[0].description}"/> ${currentDay}</h2>
+        <p>Country: ${weatherResponse.sys.country}</p>
+        <p>Temperature: ${weatherResponse.main.temp} °C</p>
+        <p>Humidity: ${weatherResponse.main.humidity} \%</p>
+        <p>Wind Speed: ${weatherResponse.wind.speed} MPH</p>
+        `);
+        $("#currentWeather").append(currentCity);
+
+    });
+};
