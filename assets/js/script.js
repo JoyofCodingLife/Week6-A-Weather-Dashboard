9e1a889206f9013646da5ab5c4b08958fd23acf7
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-04-06 23:50:50 +1000

    JS - working on JS for UV

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ var currentDay = moment().format("MMM Do YY");
 var searchButton = $("#searchBtn");
 var searchHistoryList =[];
 
-
 // SEARCH SECTION
 
  // Search button event listener
@@ -36,21 +35,40 @@ var searchHistoryList =[];
         url: currentWeatherURL,
         method: "GET",
     }).then (function(weatherResponse) {
-        //console.log(weatherResponse);
+        console.log(weatherResponse);
 
         $("#resultSection").css("display", "block");
         $("#currentWeather").empty();
 
         var iconID = weatherResponse.weather[0].icon;
         var iconURL = `http://openweathermap.org/img/wn/${iconID}.png`;
+
+        // UV index requires another API data collection plus city geo coordinates
+        var lon = weatherResponse.coord.lon;
+        var lat = weatherResponse.coord.lat;
+
+        var uviURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${apiKey}`;
+        $.ajax({
+            url: uviURL,
+            method: "GET",
+        }). then (function(uviResponse) {
+            console.log(uviResponse);
+            var uvIndex = uviResponse.current.uvi;
+            console.log(uvIndex);
+        });
+
         var currentCity = $(`
         <h2 id="currentCity">${weatherResponse.name} <img src="${iconURL}" alt="${weatherResponse.weather[0].description}"/> ${currentDay}</h2>
         <p>Country: ${weatherResponse.sys.country}</p>
         <p>Temperature: ${weatherResponse.main.temp} °C</p>
         <p>Humidity: ${weatherResponse.main.humidity} \%</p>
         <p>Wind Speed: ${weatherResponse.wind.speed} MPH</p>
+        <p>UV Index: ${uvIndex}</p>
+
         `);
         $("#currentWeather").append(currentCity);
 
+
     });
+    
 };
